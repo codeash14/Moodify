6cab9cb8f80c8198dd871eef1ae44e43df75eab3
ayushi-g <ayushi.gupta@symbtechnologies.com>
2023-05-08 16:56:35 +0530

    dev commit

--- moodify_webapp/authorization_code/server.js
+++ moodify_webapp/authorization_code/server.js
@@ -1,16 +1,15 @@
 /**
  * A script that performs the Authorization Code oAuth2 flow to authenticate against the Spotify Accounts.
  */
-
 let express = require('express'); // Express web server framework
 let request = require('request'); // "Request" library
 let cors = require('cors');
 let querystring = require('querystring');
 let cookieParser = require('cookie-parser');
-
-let client_id = "0edbf3e5ef584779b50569360db2c2bb"; // Your client id
-let client_secret = "080b76427e6a4e5fa88102f959c99e3d"; // Your secret
-let redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+require('dotenv').config();
+let client_id = process.env.CLIENT_ID; // Your client id
+let client_secret = process.env.CLIENT_SECRET; // Your secret
+let redirect_uri = process.env.REDIRECT_URI; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
@@ -62,8 +61,7 @@ app.get('/callback', function (req, res) {
   let state = req.query.state || null;
   let storedState = req.cookies ? req.cookies[stateKey] : null;
   // we can also pass the token to the browser to make requests from there
-  let uri = process.env.FRONTEND_URI || 'http://127.0.0.1:5000/#'
-  console.log("uri: " + process.env.FRONTEND_URI)
+  let uri = process.env.FRONTEND_URI
 
   if (state === null || state !== storedState) {
     res.redirect(uri +
@@ -140,6 +138,6 @@ app.get('/refresh_token', function (req, res) {
   });
 });
 
-let port = process.env.PORT || 8888
+let port = process.env.PORT
 console.log(`Listening on port http://localhost:${port}. Go /login to initiate authentication flow.`)
 app.listen(port)
